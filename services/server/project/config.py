a3aadedbe1c6311f5d28a41ba6214ba9fe4abb4e
Michael Herman <hermanmu@gmail.com>
2018-09-09 15:07:03 -0600

    adds kubernetes config files

--- services/server/project/config.py
+++ services/server/project/config.py
@@ -1,5 +1,9 @@
 import os
 
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+DATABASE_URL = f'postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres:5432/books'
+
 
 class BaseConfig:
     """Base configuration"""
@@ -10,9 +14,9 @@ class BaseConfig:
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
