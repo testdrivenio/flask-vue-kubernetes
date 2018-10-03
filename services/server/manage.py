6ce2dc6bdab5d87b2807564a2a1c772751f66837
Michael Herman <hermanmu@gmail.com>
2018-10-02 22:00:00 -0600

    updates management commands

--- services/server/manage.py
+++ services/server/manage.py
@@ -8,14 +8,14 @@ app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
 
-@cli.command()
+@cli.command('recreate_db')
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
 
 
-@cli.command()
+@cli.command('seed_db')
 def seed_db():
     """Seeds the database."""
     db.session.add(Book(
